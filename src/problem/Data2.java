2d48c07a2bbe15ef641e9b84c842fc7be9131edc
dbsrjsdn0130 <ygw130@naver.com>
2024-11-18 00:29:55 +0900

    data2

---
+++ src/problem/Data2.java
@@ -0,0 +1,18 @@
+package problem;
+
+class Data2 { int x; }
+
+class Ex6_7 {
+    public static void main(String[] args) {
+        Data2 d = new Data2();
+        d.x=10;
+        System.out.println("main(): x = "+d.x);
+        change(d);
+        System.out.println("After change(d)");
+        System.out.println("main() : x = "+d.x);
+    }
+    static void change(Data2 d) {
+        d.x=1000;
+        System.out.println("change() : x = " +d.x);
+    }
+}
